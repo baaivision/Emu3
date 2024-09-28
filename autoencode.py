a73d1ae3944bb475abcd1bfc1902bcd77fcf80d4
Fan Zhang <zhangfan@baai.ac.cn>
2024-09-28 11:07:28 +0800

    add autoencode example codes

---
+++ autoencode.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import os
+import os.path as osp
+
+from PIL import Image
+import torch
+from transformers import AutoModel, AutoImageProcessor
+
+MODEL_HUB = "BAAI/Emu3-VisionTokenizer"
+
+model = AutoModel.from_pretrained(MODEL_HUB, trust_remote_code=True).eval().cuda()
+processor = AutoImageProcessor.from_pretrained(MODEL_HUB, trust_remote_code=True)
+
+# TODO: you need to modify the path here
+VIDEO_FRAMES_PATH = "YOUR_VIDEO_FRAMES_PATH"
+
+video = os.listdir(VIDEO_FRAMES_PATH)
+video.sort()
+video = [Image.open(osp.join(VIDEO_FRAMES_PATH, v)) for v in video[:16]]
+
+images = processor(video, return_tensors="pt")["pixel_values"]
+images = images.unsqueeze(0).cuda()
+
+# image reconstruction
+image = images[:, 0]
+print(image.shape)
+with torch.no_grad():
+    codes = model.encode(image)
+    recon = model.decode(codes)
+
+recon = recon.view(-1, *recon.shape[2:])
+recon_image = processor.postprocess(recon)["pixel_values"][0]
+recon_image.save("recon_image.png")
+
+# video reconstruction
+images = images.view(
+    -1,
+    model.config.temporal_downsample_factor,
+    *images.shape[2:],
+)
+
+print(images.shape)
+with torch.no_grad():
+    codes = model.encode(images)
+    recon = model.decode(codes)
+
+recon = recon.view(-1, *recon.shape[2:])
+recon_images = processor.postprocess(recon)["pixel_values"]
+for idx, im in enumerate(recon_images):
+    im.save(f"recon_video_{idx}.png")
